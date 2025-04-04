89ace895e1b2ed6631f8ca301e9caa0f423a674f
RP <info@syntorion.com>
2025-04-04 12:32:14 +0530

    feat: Fetcher for the server and client implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { serverFetcher } from '@/lib/api-hub';
+
+export default async function Home() {
+    const res = await serverFetcher('https://jsonplaceholder.typicode.com/posts/1');
+    console.log(res);
+
     return <div>Homepage</div>;
 }
