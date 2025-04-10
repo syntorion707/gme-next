82dbd43205867afedf5ed097d184b060d7ab562a
Dinesh-genius <ramkumar2508.e@gmail.com>
2025-04-10 18:23:00 +0530

    feat: Featured brands page and shop all brands page

---
+++ src/app/allbrands/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShopAllBrands from '@/components/categories-page/shop-all-brands';
+
+const allBrands = () => {
+    return (
+        <div>
+            <ShopAllBrands />
+        </div>
+    );
+};
+
+export default allBrands;
