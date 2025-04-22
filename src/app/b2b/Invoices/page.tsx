056f9d6d3e458b951ff18ac2a822f51baa741417
Vishnu VV <vishnusparrow26@gmail.com>
2025-04-22 11:32:14 +0530

    feat: The Group History and the Invoices Pay pages has been added.

---
+++ src/app/b2b/Invoices/page.tsx
@@ -0,0 +1,10 @@
+import Invoices from '@/components/gme/b2b/Invoices';
+
+const InvoicePage = () => {
+    return (
+        <div>
+            <Invoices />
+        </div>
+    );
+};
+export default InvoicePage;
