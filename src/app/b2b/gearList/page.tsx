47cc37b9a63b20127f70df17dadf5508bf8beae7
Dinesh-genius <ramkumar2508.e@gmail.com>
2025-04-24 15:49:59 +0530

    fix: Changed the variable name

--- src/app/b2b/gearList/page.tsx
+++ src/app/b2b/gearList/page.tsx
@@ -2,9 +2,9 @@ import { GearList } from '@/components/gme/b2b';
 import { getGearList } from '@/lib/api-hub/server-actions';
 
 const GearListPage = async () => {
-    const gearList = await getGearList();
-    
-return <GearList data={gearList} />;
+    const res = await getGearList();
+
+    return <GearList data={res} />;
 };
 
 export default GearListPage;
